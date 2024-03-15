9f2b5b8c258428a0621ebcfe7d6b70e5ced39691
origin <bhuvaneswarjakka99@gmail.com>
2024-03-15 12:58:42 +0530

    first

---
+++ TaskManager_Spring_Java/src/test/java/com/Bhuvaneswar/TaskManager_Spring_Java/TaskManagerSpringJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bhuvaneswar.TaskManager_Spring_Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerSpringJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
